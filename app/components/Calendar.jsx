8a61561a640003c58b9fd626c0d8e04d2dd3d92a
github username <github email address>
2016-05-03 15:33:32 -0400

    parsing JSON file

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -2,7 +2,17 @@ import AltContainer from 'alt-container';
 import React from 'react';
 import { Link } from 'react-router';
 import moment from 'moment';
+
+var customData = require('./data.json');
+console.log("This" + customData);
+
 export default class Calendar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+}
+
 
   render() {
 
@@ -11,18 +21,29 @@ export default class Calendar extends React.Component {
       <div className="amContainer">
         <div className="amSide">AM</div>
         <div className="linesContainer">
-          <div className="hour">
+          <div className="hourBlock">
               <div className="time">
               {moment().format('9:00')}
               </div>
               <div className="halfHour">
+                {moment().format('9:30')}
               </div>
           </div>
-          <div className="hour">
-          {moment().format('10:00')}
+          <div className="hourBlock">
+            <div className="time">
+            {moment().format('10:00')}
+            </div>
+            <div className="halfHour">
+              {moment().format('10:30')}
+            </div>
           </div>
-          <div className="hour">
-          {moment().format('11:00')}
+          <div className="hourBlock">
+            <div className="time">
+            {moment().format('11:00')}
+            </div>
+            <div className="halfHour">
+              {moment().format('11:30')}
+            </div>
           </div>
         </div>
       </div>
